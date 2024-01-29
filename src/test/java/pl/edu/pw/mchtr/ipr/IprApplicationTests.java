3c8bbe17d6f1752d230f21af0d97bb7a05ec2cbb
Bernard Pokorski <01162883@pw.edu.pl>
2024-01-29 19:32:20 +0100

    first commit

---
+++ src/test/java/pl/edu/pw/mchtr/ipr/IprApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.mchtr.ipr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
